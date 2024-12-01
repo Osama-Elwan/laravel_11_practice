dafd5477749430b317fd7817a5d40f503e426757
Osama-Elwan <osama.elwan01@gmail.com>
2024-12-01 21:16:00 +0200

    end of migraion videos

---
+++ taskThree/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SingleActionController;
+use App\Models\Blog;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [HomeController::class,'index']);
+Route::get('/about',[ HomeController::class,'showAboutPage']);
+Route::get('/single-action', SingleActionController::class);
+
+
+// Route::resource('/blog', BlogController::class);
+
+Route::get('/blog',function() {
+    $blogs = Blog::all();//select * form bolgs //retrun colleciton
+    // return $blogs;
+    dd($blogs);
+});
